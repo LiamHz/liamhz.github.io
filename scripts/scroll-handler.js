d4147ca8200ff7cfc8614dd61b7c367026d3e772
LiamHz <liam.hinzman@gmail.com>
2023-03-18 02:05:24 -0400

    Update js and css for fave page changes

--- scripts/scroll-handler.js
+++ scripts/scroll-handler.js
@@ -1,21 +1,21 @@
-const monthContainers = document.getElementsByClassName("month-container");
+const sectionContainers = document.getElementsByClassName("section-container");
 
-// On scroll, check which month container is in view
+// On scroll, check which section container is in view
 // Set the corresponding nav item to active
 window.addEventListener('scroll', function(ev) {
-  // Check distance from viewport top of each month container
-  Array.from(monthContainers).forEach(e => {
+  // Check distance from viewport top of each sectoin container
+  Array.from(sectionContainers).forEach(e => {
     const distTop = e.getBoundingClientRect().top;
     if (distTop > 0 && distTop < window.innerHeight/2) {
       // Unset active status from nav elements
-      const navMonths = document.getElementsByClassName("nav-month"); 
-      Array.from(navMonths).forEach(n => n.classList.remove("active"))
+      const navItems = document.getElementsByClassName("nav-item"); 
+      Array.from(navItems).forEach(n => n.classList.remove("active"))
 
       // Set active status
-      const activeMonth = e.getAttribute("id")
-      const activeNav = document.getElementById(`l-${activeMonth}`);
+      const activeItem = e.getAttribute("id")
+      const activeNav = document.getElementById(`l-${activeItem}`);
       activeNav.classList.add("active")
-      //window.location.hash = `#${activeMonth}`;
+      //window.location.hash = `#${activeItem}`;
     }
   })
 });
