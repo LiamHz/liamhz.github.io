f756658082a7b46a2f301800f8524e918724a311
Liam Hinzman <liam.hinzman@gmail.com>
2023-07-07 19:33:31 +0000

    Artwork gallery layout and influences section

---
+++ scripts/homepage.js
@@ -0,0 +1,23 @@
+[...document.getElementsByTagName("details")].forEach( (D,_,A) =>
+  D.addEventListener("toggle", E =>
+    D.open && A.forEach(d =>
+      d!=E.target && (d.open=false)
+    )
+  )
+)
+
+const changeInfluenceContent = contentId => {
+  const prevActiveNavItem = document.querySelector("#influences nav a.active")
+  const targetNavItem = document.querySelector(`#influences nav a.${contentId}`)
+
+  const prevActiveContent = document.getElementsByClassName("active-influence-content")[0]
+  const targetContent = document.getElementById(contentId)
+
+  prevActiveNavItem.classList.remove("active")
+  targetNavItem.classList.add("active")
+
+  prevActiveContent.classList.remove("active-influence-content")
+  targetContent.classList.add("active-influence-content")
+}
+
+// TODO Define function changeDisplayImage
